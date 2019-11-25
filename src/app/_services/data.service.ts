c4cc6917b0328bd588c7b84031ee73b49c448ca4
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-11-25 21:51:36 +0530

    Finished

--- src/app/_services/data.service.ts
+++ src/app/_services/data.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  constructor() {
-    console.log('Service created');
-  }
+  stream$ = new Observable<number>(observer => {
+    let counter = 0;
+    setInterval(() => {
+      observer.next(counter);
+      counter++;
+    }, 1000);
+  });
+  constructor() {}
 }
