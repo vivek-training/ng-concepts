0df0a06c3f1126c02fa20e703fc464fcc3a82f1c
vivekanandpv-mac <vivekanandpv@outlook.com>
2019-11-25 22:01:46 +0530

    finished

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../_services/data.service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +11,10 @@ import { map } from 'rxjs/operators';
 export class NavbarComponent implements OnInit {
   compStream$: Observable<number>;
   constructor(private dataService: DataService) {
-    this.compStream$ = dataService.stream$.pipe(map(v => v * v));
+    this.compStream$ = dataService.stream$.pipe(
+      map(v => v * v),
+      tap(val => console.log('Tapping to value', val))
+    );
   }
 
   ngOnInit() {}
